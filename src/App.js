f9b0268f4dfa1076a83e62b674a65d39db14191c
dallasgreene <rkdaproxy@gmail.com>
2019-12-05 16:14:21 -0500

    added font awesome icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import { createStore } from "redux";
-import SearchPageReducer from "./reducers/SearchPageReducer";
 import { BrowserRouter as Router } from "react-router-dom";
 import HomePageContainer from "./containers/HomePageContainer";
 import { Provider } from "react-redux";
 import HomePageReducer from "./reducers/HomePageReducer";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faPlus, faGripHorizontal, faSortAlphaUp, faList, faEdit, faTimes, faArrowUp, faArrowDown }
+    from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars, faPlus, faGripHorizontal, faSortAlphaUp, faList, faEdit, faTimes, faArrowUp, faArrowDown);
 
 const App = () => {
     const store = createStore(HomePageReducer);
