1ae3f8ef897ec79d33e161af4cad9d98ba149f3d
dallasgreene <rkdaproxy@gmail.com>
2019-12-09 19:43:12 -0500

    user CRUD operations added

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,4 +1,4 @@
-const rootUrl = "";
+const rootUrl = "https://wbdv-t18-server-node.herokuapp.com/api";
 
 export default class UserService {
     static myInstance = null;
@@ -10,15 +10,56 @@ export default class UserService {
         return this.myInstance
     }
 
+    findAllUsers = () => {
+        return fetch(`${rootUrl}/users`)
+            .then(response => response.json());
+    };
+
+    findUserById = userId => {
+        return fetch(`${rootUrl}/users/${userId}`)
+            .then(response => response.json());
+    };
+
     login = (username, password) => {
-        return fetch(
-            `${rootUrl}/api/users`,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }
-        ).then(response => response.json());
+        return fetch(`${rootUrl}/users/login`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+            .then(response => response.json());
+    };
+
+    createUser = user => {
+        return fetch(`${rootUrl}/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => response.json());
+    };
+
+    updateUser = user => {
+        return fetch(`${rootUrl}/users`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => response.json());
+    };
+
+    deleteUser = userId => {
+        return fetch(`${rootUrl}/users/${userId}`, {
+            method: 'DELETE'
+        })
+            .then(response => response.json());
     };
 }
